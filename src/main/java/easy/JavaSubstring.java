8ebbcecdb0bb98561babc31f44084b0ebe397d2d
hardeep85singh <hardeep85singh@gmail.com>
2021-04-15 20:59:40 +0530

    commit on 15.04.21

---
+++ src/main/java/easy/JavaSubstring.java
@@ -0,0 +1,16 @@
+package easy;
+
+import java.util.Scanner;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        String substring = S.substring(start, end);
+        System.out.println(substring);
+    }
+}
